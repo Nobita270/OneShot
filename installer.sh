96bd40fdc84de39684b43b62a7ed868583b31100
RUOK67 <99482630+Nobita270@users.noreply.github.com>
2024-01-07 22:42:14 +0600

    Update installer.sh
    Link and command updated

--- installer.sh
+++ installer.sh
@@ -3,22 +3,22 @@ apt remove -y game-repo science-repo
 pkg install -y root-repo
 pkg install -y git tsu python wpa-supplicant pixiewps iw
 
-git clone --depth 1 https://github.com/Lunatik-cyber/OneShot OneShot
+git clone --depth 1 https://github.com/Nobita270/OneShot OneShot
 
 chmod +x /data/data/com.termux/files/home/OneShot/oneshot.py
 chmod +x /data/data/com.termux/files/home/OneShot/wifi_connect.sh
-echo "cd /data/data/com.termux/files/home/OneShot; sudo python3 oneshot.py -i wlan0 -K -w" > /data/data/com.termux/files/usr/bin/hack
-echo "clear; cat /data/data/com.termux/files/home/OneShot/reports/stored.txt" > /data/data/com.termux/files/usr/bin/hacked
-chmod +x /data/data/com.termux/files/usr/bin/hack
-chmod +x /data/data/com.termux/files/usr/bin/hacked
+echo "cd /data/data/com.termux/files/home/OneShot; sudo python3 oneshot.py -i wlan0 -K -w" > /data/data/com.termux/files/usr/bin/wifi
+echo "clear; cat /data/data/com.termux/files/home/OneShot/reports/stored.txt" > /data/data/com.termux/files/usr/bin/wified
+chmod +x /data/data/com.termux/files/usr/bin/wifi
+chmod +x /data/data/com.termux/files/usr/bin/wified
 clear
 printf "#########################################################
 #  All done! Now you can run OneShot with:
-#   sudo python3 OneShot/oneshot.py -i wlan0 -K or hack
+#   sudo python3 OneShot/oneshot.py -i wlan0 -K or wifi
 #
 #  To update, run:
 #   (cd OneShot && git pull)
 #
 #  To see hacked networks:
-#    hacked
+#    wified
 #########################################################\n"
