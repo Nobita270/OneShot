4ddfbcf980fe248410e79be8225bef1de2531bac
RUOK67 <99482630+Nobita270@users.noreply.github.com>
2024-07-25 16:36:53 +0600

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -9,16 +9,16 @@ chmod +x /data/data/com.termux/files/home/OneShot/oneshot.py
 chmod +x /data/data/com.termux/files/home/OneShot/wifi_connect.sh
 echo "cd /data/data/com.termux/files/home/OneShot; sudo python3 oneshot.py -i wlan0 -K -w" > /data/data/com.termux/files/usr/bin/wifi
 echo "clear; cat /data/data/com.termux/files/home/OneShot/reports/stored.txt" > /data/data/com.termux/files/usr/bin/waifus
-chmod +x /data/data/com.termux/files/usr/bin/wifi
-chmod +x /data/data/com.termux/files/usr/bin/waifus
+chmod +x /data/data/com.termux/files/usr/bin/?waifu
+chmod +x /data/data/com.termux/files/usr/bin/?waifus
 clear
 printf "#########################################################
 #  All done! Now you can run OneShot with:
-#   sudo python3 OneShot/oneshot.py -i wlan0 -K or wifi
+#   sudo python3 OneShot/oneshot.py -i wlan0 -K or ?waifu
 #
 #  To update, run:
 #   (cd OneShot && git pull)
 #
 #  To see hacked networks:
-#    waifus
+#    ?waifus
 #########################################################\n"
